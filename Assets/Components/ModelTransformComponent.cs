e00418133ceb45fec7596424cee9493d269d3e18
ronald112 <ronald11211@gmail.com>
2022-04-05 20:56:39 +0100

    add modelTransform

---
+++ Assets/Components/ModelTransformComponent.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+namespace Client
+{
+    [Serializable]
+    public struct ModelTransformComponent
+    {
+        public Transform transform;
+    }
+}
---
+++ Assets/Components/ModelTransformProvider.cs
@@ -0,0 +1,9 @@
+using Voody.UniLeo.Lite;
+
+namespace Client
+{
+    public class ModelTransformProvider : MonoProvider<ModelTransformComponent>
+    {
+        
+    }
+}
